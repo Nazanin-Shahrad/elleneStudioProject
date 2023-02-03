d4afaf285e758d07002766e0b73ecad909671934
Nazanin-Shahrad <nazanin.shahrad@gmail.com>
2023-02-03 10:26:54 -0500

    add styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
+import {useEffect , useState } from 'react';
 
-import './App.css';
+//components
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Loader from "./components/Loader";
+
 
 function App() {
+
+  const [loading , setLoading] = useState(true);
+
+  useEffect(() => {
+    loading
+    ? document.querySelector("body").classList.add("loading")
+    : document.querySelector("body").classList.remove("loading");
+  },[loading])
+
+
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    {loading ? (
+      <Loader setLoading={setLoading} />
+    ) : (
+      <>
+        <Header />
+        <Banner />
+        {!loading && (
+          <div className='transition-image final'>
+            <img src={`/images/image-2.jpg`} alt=""/>
+          </div>
+        )}
+      </>
+     )}
+  
+    </>
   );
 }
 
